8228a9a4114466d572002b820a1e49acf61183f3
liuyongkui <liuyongkui726@pingan.com.cn>
2016-08-09 16:50:49 +0800

    add BaseResponse 增加网络返回基类

---
+++ app/src/main/java/com/tamic/retrofitclient/net/BaseResponse.java
@@ -0,0 +1,42 @@
+package com.tamic.retrofitclient.net;
+
+
+/**
+ * 网络返回基类 支持泛型
+ * Created by Tamic on 2016-06-06.
+ */
+public class BaseResponse<T> {
+
+    private int code;
+    private String msg;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isOk() {
+        return code == 0;
+    }
+
+}
